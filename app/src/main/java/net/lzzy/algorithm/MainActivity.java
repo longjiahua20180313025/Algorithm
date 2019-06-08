d9232f79d87fd46beee1d0b5e50bff305cb12bac
Administrator <895885871@qq.com>
2019-06-08 16:01:45 +0800

    直接选着排序

--- app/src/main/java/net/lzzy/algorithm/MainActivity.java
+++ app/src/main/java/net/lzzy/algorithm/MainActivity.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.util.Calendar;
 import java.util.Random;
 
 /**
@@ -53,7 +52,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void directSort() {
+        //分别有序区和无序区，每一趟排序都在无序区做次对比，记录对比区域的最小元素的位置
+        //然后把无序区第一个元素和所有记录的最小元素进行交易，有区域多一个，循环往复直至无直区
+        //元数数量为0
         //todo:直接选择排序的具体实现
+
+//        int num = 10;
+//        for (int i = 0; i <= num - 1; i++) {
+//            for (int j = 0; j < num - 1; j++) {
+//                if (items[j] < items[j + 1]) {
+//                    //当前的数比后面的数大时
+//                    {
+//                        int temp;//定义临时变量变量
+//                        temp = items[j];
+//                        items[j] = items[j + 1];
+//                        items[j + 1] = temp;
+//                        //比较大的数放在后面
+//                    }
+//                }
+//            }
+//        }
+        for (int i = 0; i < items.length - 1; i++) {
+            int ninpos = i;
+            for (int j = i + 1; j < items.length; j++) {
+                if (items[ninpos].compareTo(items[j]) > 0) {
+                    ninpos = j;
+                }
+            }
+            swap(ninpos, i);
+        }
+    }
+
+    private void swap(int i, int j){
+        int temp;//定义临时变量变量
+        temp = items[i];
+        items[i] = items[j];
+        items[j] = temp;
     }
 
     private void generateItems() {
